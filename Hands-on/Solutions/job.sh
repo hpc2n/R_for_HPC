1192e0421666f5d395648260f4c801001473a591
Pedro Ojeda-May <pedroojeda2011@googlemail.com>
2021-02-10 20:44:10 +0100

    RHPC 4th example challenge

---
+++ Hands-on/Solutions/job.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -A *FIXME*
+#Asking for 7 min.
+#SBATCH -t 00:07:00
+#SBATCH -c 2
+
+ml purge
+ml GCC/8.2.0-2.31.1  OpenMPI/3.1.3
+ml R/3.6.0 
+
+R --no-save --no-restore -f script1.R
+#R --no-save --no-restore -f script2.R
